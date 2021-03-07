84cd7a2c3d56d202bbf73a04e21796b3aec2c0df
Hazael Jiménez <viajero25057@innovaccion.mx>
2021-03-06 23:30:21 -0600

    feature: Installation and configuration of fonts

--- App.js
+++ App.js
@@ -2,15 +2,49 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {tailwind} from './lib/tailwind';
+import { 
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from '@expo-google-fonts/roboto'
+import { 
+  Poppins_600SemiBold,
+  useFonts
+} from '@expo-google-fonts/poppins'
 
+//Definiendo las fuentes que usaremos en el proyecto
+const fonts = {
+  title: {
+    fontFamily:'Poppins_600SemiBold',
+    fontSize: 60,
+  },
+  subtitle: {
+    fontFamily:'Roboto_500Medium',
+    fontSize: 32,
+  },
+  body: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+  },
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>¡Agregando tailwind! 👋</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  //Cargando las fuentes con el Hook
+  let [fontsLoaded] = useFonts({
+    Poppins_600SemiBold, 
+    Roboto_500Medium, 
+    Roboto_400Regular,
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }else{
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>¡Agregando tailwind! 👋</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -21,6 +55,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text:{
-    ...tailwind('bg-blue-500 px-5 py-3 rounded-full text-white font-semibold text-lg'),
+    ...tailwind('bg-blue-500 px-5 py-3 rounded-full text-white'),
+    ...fonts.body
   }
 });
